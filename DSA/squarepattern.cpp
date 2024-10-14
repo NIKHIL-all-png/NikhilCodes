2541ca929f51d5f815f79c3cb180ecf32daf1594
NIKHIL GUPTA <nkgt9170413117@gmail.com>
2024-10-15 01:24:02 +0530

    commited

--- DSA/squarepattern.cpp
+++ DSA/squarepattern.cpp
@@ -8,9 +8,10 @@ int main() {
     int num=1;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cout<<num;
+            cout<<num<<" ";
             num++;
         }
         cout<<endl;
     }
+    return 0;
 }
