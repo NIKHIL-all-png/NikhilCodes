15ea46e75df207219b6aab9552b7eed7338e251d
NIKHIL GUPTA <nkgt9170413117@gmail.com>
2024-10-23 20:04:07 +0530

    commityed

--- DSA/Prime.cpp
+++ DSA/Prime.cpp
@@ -2,11 +2,19 @@
 
 using namespace std;
 
-int main() {
-    int n;
+int primeORnot(int num){
     bool isprime=true;
-    cout<< n<<endl;
-    cin>>n;
+    if(num<=1){
+        return false;
+    }
+    for(int i=2;i<num;i++){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
 
-    
+int main(){
+    cout<<primeORnot(3)<<endl;
 }
