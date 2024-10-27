209d6b5057a3174ae89e8b24ec82bbfa86b8913b
NIKHIL GUPTA <nkgt9170413117@gmail.com>
2024-10-28 02:07:33 +0530

    commited

--- DSA/ARRAYS/Lecture1.o.cpp/linearsearch1.cpp
+++ DSA/ARRAYS/Lecture1.o.cpp/linearsearch1.cpp
@@ -1,3 +1,23 @@
 #include<iostream>
 using namespace std;
 
+int linearsearch(int arr[], int size,int target){
+    for(int i=0; i<size; i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+
+}
+
+
+int main(){
+
+    int arr[] = {4, 2, 7, 8, 1, 2, 5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int target=5;
+
+    cout<<linearsearch(arr, size,target);
+    return 0;
+}
