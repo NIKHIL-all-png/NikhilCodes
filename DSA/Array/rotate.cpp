eaf20d84e9e57e26286e7182673b3c6c9622c7bf
NIKHIL GUPTA <nkgt9170413117@gmail.com>
2025-01-01 21:28:00 +0530

    commited

--- DSA/Array/rotate.cpp
+++ DSA/Array/rotate.cpp
@@ -2,18 +2,21 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-void rotate(int arr[],int n){
+void rotate(int nums[],int n){
     int s=0;
     int e=n-1;
     while(s<e){
-        swap(arr[s],arr[e]);
+        swap(nums[s],nums[e]);
         s++;
         e--;
     }
+    for(int i=0;i<n;i++){
+        cout<<nums[i];
+    }
 }
 int main (){
-    int arr[5]={1,2,3,4,5};
-    rotate(arr,5);
+    int nums[5]={1,2,3,4,5};
+    rotate(nums,5);
     return 0;
 
 }
